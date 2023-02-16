dc220bd6fd24eb21a7b6281e77edc962a3056b6b
bungaalfa005 <109193542+bungaalfa005@users.noreply.github.com>
2023-02-16 13:49:04 +0700

    Add files via upload

---
+++ Praukk_16feb/db_praukk16feb_bunga.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Feb 2023 pada 07.45
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_praukk16feb_bunga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_bahan`
+--
+
+CREATE TABLE `tb_bahan` (
+  `id_bahan` int(10) NOT NULL,
+  `nama_bahan` varchar(30) NOT NULL,
+  `jenis_bahan` varchar(50) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `kondisi` enum('baik','rusak') NOT NULL,
+  `tgl_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_bahan`
+--
+
+INSERT INTO `tb_bahan` (`id_bahan`, `nama_bahan`, `jenis_bahan`, `stok`, `harga`, `kondisi`, `tgl_masuk`) VALUES
+(4, 'LIGHTSTICK', 'KAYU', 1, 10000, '', '2023-02-13'),
+(5, 'LIGHTSTICK', 'KAYU', 32, 23, '', '2023-02-07'),
+(7, 'f', 'fg', 1, 1, 'baik', '2023-02-11'),
+(8, 'LIGHTSTICK', 'KAYU', 12, 10000, 'baik', '2023-02-16'),
+(10, 'BUKU', 'KERTAS', 5, 10000, 'baik', '0000-00-00'),
+(11, 'BUKU', 'KERTAS', 5, 10000, 'baik', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(10) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `id_bahan` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_user`, `jabatan`, `id_bahan`) VALUES
+(11, 'BUNGA', 'SISWA', 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  ADD PRIMARY KEY (`id_bahan`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_bahan` (`id_bahan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  MODIFY `id_bahan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_bahan`) REFERENCES `tb_bahan` (`id_bahan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Praukk_16feb/index.php
@@ -0,0 +1,21 @@
+<?php
+include("koneksi.php");
+?>
+
+<html>
+    <head>
+</head>
+        <center>
+            <body>
+                <h1>APLIKASI INTERVARIS DAN BAHAN</h1>
+                <h1>LAB RPL SMK NEGERI 1 SAYUNG</h1>
+                <tr>
+                    <td>
+                        <ul>
+                            <li><h4><a href="tampil.php">lihat Data</a></h4></li>
+                            <li><h4><a href="tambah.php">tambah Data</a></h4></li>
+                        </ul>
+                    </td>
+                </tr>
+            </body>
+</html>
